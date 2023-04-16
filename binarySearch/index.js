465e03e3713b122cdf821df44767941080def861
[CORIZE] Tan Le <tan.b4pt.le@misumi.co.jp>
2023-04-16 11:06:44 +0700

    Recursive binary search

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -1,18 +1,35 @@
 const binarySearch = (arr, value) => {
-  const n = arr.length;
-  let iLeft = 0;
-  let iRight = n - 1;
-  while (iLeft <= iRight) {
-    const iMid = Math.round((iLeft + iRight) / 2);
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const iMid = Math.round((left + right) / 2);
     if (arr[iMid] === value) {
       return iMid;
-    } else if (arr[iMid] < value) {
-      iLeft = iMid + 1;
+    }
+    if (arr[iMid] > value) {
+      right = iMid - 1;
     } else {
-      iRight = iMid - 1;
+      left = iMid + 1;
     }
   }
+
   return -1;
 };
 
-const a = [1, 2, 4, 5, 6, 7, 9, 10, 11];
+const binarySearchRecursive = (arr, value, left, right) => {
+  const iMid = Math.round((left + right) / 2);
+  if (left > right) {
+    return -1;
+  }
+  if (arr[iMid] === value) {
+    return iMid;
+  } else if (arr[iMid] > value) {
+    return binarySearchRecursive(arr, value, left, iMid - 1);
+  } else {
+    return binarySearchRecursive(arr, value, iMid + 1, right);
+  }
+};
+
+const a = [2, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+console.log(binarySearchRecursive(a, 11, 0, a.length - 1));
